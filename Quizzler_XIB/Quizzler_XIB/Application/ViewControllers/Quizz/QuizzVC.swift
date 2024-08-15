f95bf1fab0695c9744ff35aa0735410b5afd5062
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-08-15 11:46:02 +0700

    Chore: initial setup

---
+++ Quizzler_XIB/Quizzler_XIB/Application/ViewControllers/Quizz/QuizzVC.swift
@@ -0,0 +1,22 @@
+//
+//  QuizzVC.swift
+//  Quizzler_XIB
+//
+//  Created by Huy on 15/8/24.
+//
+
+import UIKit
+
+class QuizzVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+    }
+}
+
+extension QuizzVC {
+    private func setup() {
+        view.backgroundColor = UIColor.red
+    }
+}
