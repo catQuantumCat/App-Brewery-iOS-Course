ff284d0183965d9b6c3c5d9189c9bb673f40656b
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-09-23 21:19:57 +0700

    Chore: pull app

---
+++ ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    let coinManager: CoinManager = CoinManager()
+
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+}
+
+
+extension ViewController : UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        coinManager.currencyArray.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        coinManager.currencyArray[row]
+    }
+    
+    
+}
+
+extension ViewController{
+    private func setup(){
+        self.currencyPicker.delegate = self;
+    }
+}
+
