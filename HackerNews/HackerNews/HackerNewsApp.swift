94a2d02d2f66456a3f5df014e3f5fee7500c4438
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-10-18 09:52:51 +0700

    Init project

---
+++ HackerNews/HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Huy on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
