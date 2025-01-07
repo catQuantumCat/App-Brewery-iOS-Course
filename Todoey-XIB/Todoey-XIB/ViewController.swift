7f89f800cb13b4cd2e9664ed9da23ee7c8108072
Tran Hoang Phuc Huy <tranhoangphuchuy04@gmail.com>
2025-01-07 16:26:28 +0700

    Chore: remove  usage

---
+++ Todoey-XIB/Todoey-XIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey-XIB
+//
+//  Created by Huy on 7/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
---
+++ Todoey-XIB/Todoey-XIBTests/Todoey_XIBTests.swift
@@ -0,0 +1,17 @@
+//
+//  Todoey_XIBTests.swift
+//  Todoey-XIBTests
+//
+//  Created by Huy on 7/1/25.
+//
+
+import Testing
+@testable import Todoey_XIB
+
+struct Todoey_XIBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
